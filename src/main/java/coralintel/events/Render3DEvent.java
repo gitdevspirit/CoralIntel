f91502b659e8c7e71e531169be5ef366577a7bdf
spirit <spirit.business15@gmail.com>
2026-08-05 17:54:48 +1000

    Create Render3DEvent.java

---
+++ src/main/java/coralintel/events/Render3DEvent.java
@@ -0,0 +1,15 @@
+package coralintel.events;
+
+import coralintel.event.events.Event;
+
+public class Render3DEvent implements Event {
+    private final float partialTicks;
+
+    public Render3DEvent(float partialTicks) {
+        this.partialTicks = partialTicks;
+    }
+
+    public float getPartialTicks() {
+        return this.partialTicks;
+    }
+}
